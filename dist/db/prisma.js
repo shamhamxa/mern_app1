e5922cfd2eadb74e145ed47889a02931cf01e40f
shamhamxa <hamxaalifi@gmail.com>
2026-01-20 10:50:15 +0500

    fix: prisma generate before tsc

--- dist/db/prisma.js
+++ dist/db/prisma.js
@@ -6,7 +6,27 @@ const client_1 = require("@prisma/client");
 const adapter_pg_1 = require("@prisma/adapter-pg");
 const pg_1 = require("pg");
 const pool = new pg_1.Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
 });
+/**
+ * 🔥 Force a connection once so Node keeps an open handle
+ * This fixes the nodemon clean-exit issue
+ */
+(async () => {
+    try {
+        const client = await pool.connect();
+        console.log("PostgreSQL pool connected and open");
+        client.release();
+    }
+    catch (err) {
+        console.error("PostgreSQL connection error:");
+        console.error(err);
+    }
+})();
 const adapter = new adapter_pg_1.PrismaPg(pool);
 exports.prisma = new client_1.PrismaClient({ adapter });
+process.on("SIGINT", async () => {
+    await exports.prisma.$disconnect();
+    await pool.end();
+    process.exit(0);
+});
--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,22 @@
 "use strict";
+// import express from "express";
+// import cors from "cors";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.get("/", (_req, res) => {
+//     res.send("ALIVE");
+// });
+// setInterval(() => {
+//     console.log("still alive");
+// }, 3000);
+// app.listen(3000, () => {
+//     console.log("Server running on http://localhost:3000");
+// });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const auth_routes_1 = require("./routes/auth.routes");
@@ -15,6 +29,6 @@ app.use((0, cors_1.default)()); // 🔥 MUST BE BEFORE ROUTES
 app.use(express_1.default.json());
 app.use("/auth", auth_routes_1.authRouter);
 app.use("/data", data_routes_1.dataRouter);
-app.listen(3000, "0.0.0.0", () => {
+app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
 });
