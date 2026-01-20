4afe468ab0642ce28c7756546dd72a1cdbdaac9d
shamhamxa <hamxaalifi@gmail.com>
2026-01-20 14:30:23 +0500

    fixed

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,7 +1,8 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
+import { PrismaClient } from "@prisma/client";
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
