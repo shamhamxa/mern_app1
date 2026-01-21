e0e4fe2d9ea85a54b05fc4ba9b2769b0764673c1
shamhamxa <hamxaalifi@gmail.com>
2026-01-21 14:46:30 +0500

    Fix Prisma 7 config (correct datasource)

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+
   migrations: {
     path: "prisma/migrations",
     seed: "ts-node prisma/seed.ts",
   },
+
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
